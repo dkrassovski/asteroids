32c5d718c3fbba2c8f486893bcdf2a7bc19a6288
Deekay <krassovski.d@gmail.com>
2025-03-26 13:37:08 -0400

    added shot

--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 from circleshape import *
 from constants import *
+from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -27,7 +28,7 @@ class Player(CircleShape):
     def shoot(self):
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOOT_SPEED      
-          
+
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
