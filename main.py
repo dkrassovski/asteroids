5a6bda59c6c9ad2eb04c413fdb1f428f17e4c946
Deekay <krassovski.d@gmail.com>
2025-03-26 09:38:35 -0400

    changed framerate

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ def main():
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -17,6 +20,8 @@ def main():
             
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(60)/1000
+
 
 
 
