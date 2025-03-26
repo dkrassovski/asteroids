2090ade77fe1cbdef7487e6be3b927ec29f94a5d
Deekay <krassovski.d@gmail.com>
2025-03-26 12:37:00 -0400

    Fixed indent

--- asteroid.py
+++ asteroid.py
@@ -6,9 +6,9 @@ class Asteroid(CircleShape):
         super().__init__(x,y)
         self.radius = radius
 
-        def draw(self, surface): 
-            pygame.draw.circle(surface, (255, 255, 255), (int(self.x), int(self.y)), self.radius, 2)
+    def draw(self, surface): 
+        pygame.draw.circle(surface, (255, 255, 255), (int(self.x), int(self.y)), self.radius, 2)
 
-        def update(self, dt):
-            self.x += self.velocity.x * dt
-            self.y += self.velocity.y * dt
+    def update(self, dt):
+        self.x += self.velocity.x * dt
+        self.y += self.velocity.y * dt
