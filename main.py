a0078092069d8ae6339db308071e320baf7cf1cc
Deekay <krassovski.d@gmail.com>
2025-03-26 09:04:33 -0400

    Beginning asteroids

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+from constants import *
+
+def main(): 
+    print (
+        "Starting Asteroids!"
+        f"Screen width: {SCREEN_WIDTH}"
+        f"Screen height: {SCREEN_HEIGHT}"
+        )
+
+if __name__ == "__main__":
+    main()
