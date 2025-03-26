9612ed7fc3daaa357dc1e55801f5b767d3b6c2b4
Deekay <krassovski.d@gmail.com>
2025-03-26 09:07:15 -0400

    edited the printing

--- main.py
+++ main.py
@@ -2,11 +2,9 @@ import pygame
 from constants import *
 
 def main(): 
-    print (
-        "Starting Asteroids!"
-        f"Screen width: {SCREEN_WIDTH}"
-        f"Screen height: {SCREEN_HEIGHT}"
-        )
+    print ("Starting Asteroids!")
+    print (f"Screen width: {SCREEN_WIDTH}")
+    print (f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
