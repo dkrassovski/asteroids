8ead346b1a4053f8062bd8717a67cf7361b5302a
Deekay <krassovski.d@gmail.com>
2025-03-26 13:01:36 -0400

    added asteroids "no collision"

--- asteroid.py
+++ asteroid.py
@@ -3,12 +3,11 @@ from circleshape import CircleShape
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
-        super().__init__(x,y)
-        self.radius = radius
+        super().__init__(x, y, radius)
 
-    def draw(self, surface): 
-        pygame.draw.circle(surface, (255, 255, 255), (int(self.x), int(self.y)), self.radius, 2)
+    def draw(self, screen): 
+        pygame.draw.circle(screen, "white", self.position, self.radius, 2)
 
     def update(self, dt):
-        self.x += self.velocity.x * dt
-        self.y += self.velocity.y * dt
+        self.position += self.velocity * dt
+  
