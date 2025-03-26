0f343cebbd882bd7be548287d73d4b9859e3d1f9
Deekay <krassovski.d@gmail.com>
2025-03-26 12:31:17 -0400

    asteroid field

--- main.py
+++ main.py
@@ -8,21 +8,32 @@ def main():
     print (f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    print("Updatable:", updatable)
+    print("Drawable:", drawable)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
         screen.fill((0, 0, 0))
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60)/1000
+        
 if __name__ == "__main__":
     main()
+
+    
--- player.py
+++ player.py
@@ -3,9 +3,9 @@ from constants import *
 
 class Player(CircleShape):
     def __init__(self, x, y):
-        super().__init__()
-        self.player_radius = PLAYER_RADIUS
+        super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -13,13 +13,17 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+    
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
+
     def rotate (self, dt):
         self.rotation = self.rotation + PLAYER_TURN_SPEED*dt
+
     def move (self,dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+        
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
